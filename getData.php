4e931669ca90948b39b658e6a5033e51b87e7096
Bruno Filipe Sousa da Silva <brunosilva_95@hotmail.com>
2018-12-29 02:03:40 +0000

    update

--- getData.php
+++ getData.php
@@ -7,18 +7,25 @@ if (isset($_GET["username"])) {
 }
 if ($summonerName) {
     $region="euw1";
+    //vai buscar o id do summoner atraves do summoner name
     $requestSummonerId = "/lol/summoner/v3/summoners/by-name/";
-    $requestChampionMastery;
-    $requestChampion;
-    $requestMatchList;
-    $requestMatch;
+    // vai buscar todos os champions organisados pelo mastery level
+    $requestChampionMastery = "/lol/champion-mastery/v3/champion-masteries/by-summoner/";
+    // vai buscar a lista dos champions
+    $requestChampion = "http://ddragon.leagueoflegends.com/cdn/6.24.1/data/en_US/champion.json";
+    //vai buscar a informaçao dos ultimos jogos com o id da conta
+    $requestMatchList = "/lol/match/v3/matchlists/by-account/";
+    // vais buscar a informaçao de um jogo em especifico com o matchId
+    $requestMatch = "/lol/match/v3/matches/";
+
     $summonerName = $_GET["username"];
-    $APIKey = "RGAPI-246bf5a7-acad-44a5-a459-d0e5d34e0ddc";
+    $APIKey = "RGAPI-4b76ecc1-3e37-4b6e-831f-894ec52ecfaa";
 
     switch ($tipoRequest) {
         
         case 'accountID':
             # code...
+
             break;
         
         case 'champion':
@@ -28,11 +35,22 @@ if ($summonerName) {
         case 'mastery':
             # code...
             break;
+        
+        case 'matchList':
+            # code...
+            break;
+
+        case 'match':
+            # code...
+            break;
+
         default:
             # code...
             break;
     }
-
+    
+    
+    
     $requestURL="https://".$region.".api.riotgames.com".$requestSummonerId.$summonerName."?api_key=".$APIKey;
     
     $data_json =file_get_contents($requestURL);
